f4e2e6c1c8cad0b00266bd83a292d45080a55927
Thoshitha Kaduruwewa <thoshithakaduru.dev@gmail.com>
2021-04-20 18:42:30 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+const App = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eaque sequi
+        suscipit at unde, nam cumque ipsum, est perspiciatis enim optio commodi
+        id fugiat molestias necessitatibus consectetur officiis error
+        consequuntur perferendis!
       </header>
     </div>
   );
-}
+};
 
 export default App;
